38983eb18108623c2fe46640305ad9f7bd927381
unknown <aluno@ifpe>
2023-11-17 10:55:58 -0300

    primeiro atividade

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import CartScreen from './CartScreen';
+import HomeScreen from './HomeScreen';
+import { MyProvider } from './MyContext';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+return (
+<MyProvider>
+<NavigationContainer>
+<Stack.Navigator>
+<Stack.Screen name="Home" component={HomeScreen} />
+<Stack.Screen name="Cart" component={CartScreen} />
+</Stack.Navigator>
+</NavigationContainer>
+</MyProvider>
+);
+};
+
+export default App;
